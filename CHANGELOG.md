c306360a5e93896ea35462122a060f4065f8fc49
nicferrari <155464561+nicferrari@users.noreply.github.com>
2025-09-01 21:54:34 +0200

    new version

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.1.3]
+- Possibility to display the list of trades produced by a strategy
+- Minor bugs fixes
--- examples/trades.rs
+++ examples/trades.rs
@@ -7,20 +7,21 @@ use rs_backtester::trades::trade_list;
 fn main() {
     let quotes = Data::new_from_yahoo("NVDA", "1d", "6mo").unwrap();
     let backtest = Backtest::new(quotes.clone(), simple_sma(quotes.clone(), 10), 100000., Commission::default());
+    println!("--------------------");
     report(backtest.clone());
     trade_list(backtest.clone());
-    backtest.to_csv("trade_list.csv").unwrap();
     let backtest_long = Backtest::new(quotes.clone(), simple_sma(quotes.clone(), 10).long_only(), 100000., Commission::default());
+    println!("--------------------");
     report(backtest_long.clone());
     trade_list(backtest_long.clone());
     let backtest_short = Backtest::new(quotes.clone(), simple_sma(quotes.clone(), 10).short_only(), 100000., Commission::default());
+    println!("--------------------");
     report(backtest_short.clone());
     trade_list(backtest_short.clone());
-    backtest_short.to_csv("trade_list.csv").unwrap();
     let mut commission = Commission::default();
     commission.rate=0.01;
     let backtest_cross = Backtest::new(quotes.clone(), sma_cross(quotes.clone(), 5, 20), 100000., commission);
+    println!("--------------------");
     report(backtest_cross.clone());
     trade_list(backtest_cross.clone());
-    backtest_cross.to_csv("trade_list.csv").unwrap()
 }
